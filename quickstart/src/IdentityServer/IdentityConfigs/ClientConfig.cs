62b910f3b4d3f92f8ea69a1181a8828e3e124f90
r.pouya <r.pouya@R-POUYA>
2022-03-12 16:52:55 +0330

    complete quick start

---
+++ quickstart/src/IdentityServer/IdentityConfigs/ClientConfig.cs
@@ -0,0 +1,46 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class ClientConfig
+{
+    public static IEnumerable<Client> GetClients()
+    {
+        return new Client[]
+        {
+            new Client
+            {
+                ClientId = "OrderServiceClient",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "InvoiceService"  , "AccountService"}
+            },
+
+            new Client
+            {
+                ClientId = "InvoiceServiceClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "OrderService"  , "AccountService"}
+            },
+            
+            new Client
+            {
+                ClientId = "AccountServiceClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedScopes = { "OrderService"  , "InvoiceService"}
+            },
+
+        };
+    }
+}
---
+++ quickstart/src/IdentityServer/IdentityConfigs/IdentityResourceConfig.cs
@@ -0,0 +1,14 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class IdentityResourceConfig
+{
+    public static IEnumerable<IdentityResource> GetIdentityResources()
+    {
+        return new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+    }
+}
