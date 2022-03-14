319718530fe54d2e0c3a45b42d6f19ac2b7d1033
r.pouya <r.pouya@R-POUYA>
2022-03-14 08:35:59 +0330

    delete useless identityServer project

--- quickstart-aspidentity/src/IdentityServer/DbContexts/PersistedGrantDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace IdentityServer.DbContexts
-//{
-
-      // PersistedGrantDbContext: used for dynamic operational data such as authorization codes and refresh tokens
-//    public class PersistedGrantDbContext
-//    {
-//    }
-//}
