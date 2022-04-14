50d24042874a3b7665f87e0f8360fa43f451c809
r.pouya <rpouya@hotmail.com>
2022-04-15 01:48:04 +0430

    reposition react-app configuration.ts

---
+++ run-aspnet-identityserver4/app/configurations.ts
@@ -0,0 +1,56 @@
+// export const configurationIdentityServer = {
+//     client_id: 'interactive.public.short', 
+//     redirect_uri: window.location.origin+'/authentication/callback', 
+//     silent_redirect_uri: window.location.origin+'/authentication/silent-callback',
+//     scope: 'openid profile email api offline_access',
+//     authority: 'https://demo.duendesoftware.com',
+//     refresh_time_before_tokens_expiration_in_second: 70,
+//     service_worker_relative_url:'/OidcServiceWorker.js',
+//     service_worker_only: false,
+//   };
+
+  export const configurationIdentityServer = {
+    client_id: 'react-app-client', 
+    //client_secret: 'secret',
+    redirect_uri: window.location.origin + '/authentication/callback', 
+    silent_redirect_uri: window.location.origin+'/authentication/silent-callback',
+    scope: 'openid profile email offline_access roles',
+    authority: 'https://localhost:5005',
+    refresh_time_before_tokens_expiration_in_second: 70,
+    service_worker_relative_url:'/OidcServiceWorker.js',
+    service_worker_only: false,
+  };
+
+// export const configurationAuth0 = {
+//     client_id: 'xGZxEAJhzlkuQUlWl90y1ntIX-0UDWHx',
+//     redirect_uri: window.location.origin+'/callback',
+//     scope: 'openid profile email api offline_access',
+//     authority: 'https://kdhttps.auth0.com',
+//     refresh_time_before_tokens_expiration_in_second: 70,
+//     service_worker_relative_url:'/OidcServiceWorker.js',
+//     service_worker_only: false,
+// };
+
+
+// export const configurationAuth0 = {
+//   client_id: 'react-app-client',
+//   redirect_uri: window.location.origin+'/callback',
+//   scope: 'openid profile email offline_access roles',
+//   authority: 'https://localhost:5005',
+//   refresh_time_before_tokens_expiration_in_second: 70,
+//   service_worker_relative_url:'/OidcServiceWorker.js',
+//   service_worker_only: false,
+// };
+
+export const configurationGoogle = {
+    client_id: '908893276222-f2drloh56ll0g99md38lv2k810d0nk0p.apps.googleusercontent.com',
+    redirect_uri: `${window.location.origin}/multi-auth/callback-google`,
+    silent_redirect_uri: window.location.origin+'/multi-auth/silent-callback-google',
+    scope: 'openid profile email',
+    authority: 'https://accounts.google.com/',
+    service_worker_relative_url:'/OidcServiceWorker.js',
+    service_worker_only: false,
+    token_request_extras: {
+        client_secret : "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"
+    }
+};
